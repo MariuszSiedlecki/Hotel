bd663e86c444d8c0045b9b029501b2470e34c623
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-03-20 00:13:56 +0100

    models

---
+++ src/main/java/mario/models/Hotel.java
@@ -0,0 +1,33 @@
+package mario.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Hotel {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @NotNull
+        private String title;
+
+        @NotNull
+        private String country ;
+
+        private String rate ;
+
+}
