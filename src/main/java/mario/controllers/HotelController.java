4b04aaa533503395b502c6ddf4a8055dcad22688
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-03-20 16:53:34 +0100

    basic version

--- src/main/java/mario/controllers/HotelController.java
+++ src/main/java/mario/controllers/HotelController.java
@@ -5,11 +5,9 @@ import mario.services.HotelService;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 
