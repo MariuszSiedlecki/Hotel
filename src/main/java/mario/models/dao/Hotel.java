97d2091368f99bfe8096df9b6fb8a37c63271930
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-03-25 00:13:58 +0100

    create models address

--- src/main/java/mario/models/dao/Hotel.java
+++ src/main/java/mario/models/dao/Hotel.java
@@ -6,11 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 
 @Getter
