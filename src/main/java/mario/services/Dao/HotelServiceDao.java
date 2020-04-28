6950b051ed212f8ac1e972e99ca4443104a3d523
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-04-28 23:45:53 +0200

    7ChangeServiceHotel

---
+++ src/main/java/mario/services/Dao/HotelServiceDao.java
@@ -0,0 +1,63 @@
+package mario.services.Dao;
+
+import mario.exceptions.ResourceNotFoundException;
+import mario.mappers.HotelMapper;
+import mario.models.dao.Hotel;
+import mario.repository.AddressRepository;
+import mario.repository.HotelRepository;
+import mario.repository.RoomRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HotelServiceDao {
+    private static final Logger logger = LoggerFactory.getLogger(HotelServiceDao.class);
+
+    private HotelRepository hotelRepository;
+    private AddressRepository addressRepository;
+    private HotelMapper hotelMapper;
+    private RoomRepository roomRepository;
+
+    public HotelServiceDao(HotelRepository hotelRepository, AddressRepository addressRepository, HotelMapper hotelMapper, RoomRepository roomRepository) {
+        this.hotelRepository = hotelRepository;
+        this.addressRepository = addressRepository;
+        this.hotelMapper = hotelMapper;
+        this.roomRepository = roomRepository;
+    }
+
+    public List<Hotel> getHotels(){
+      List<Hotel>hotels =hotelRepository.findAll();
+        logger.debug("Hotels:{}",hotels);
+        MDC.clear();
+        return hotels;
+    }
+
+    public Hotel getHotelById(Long id){
+        return hotelRepository
+                .findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Not found hotel by  id" + id));
+
+    }
+    public List<Hotel>getHotelByCountry(String country){
+        return hotelRepository.findByCountry(country);
+    }
+
+    public List<Hotel>getHotelByCountryAndRate(String country, String rate){
+        return hotelRepository.findByCountryAndRate(country,rate);
+    }
+//    ----------Dto service-------
+//
+//    public List<HotelDto> getHotelsDto(){
+//        List<Hotel>hotels =hotelRepository.findAll();
+//
+//        return hotels
+//                .stream()
+//                .map(hotelMapper::map)
+//                .collect(Collectors.toList());
+//
+//    }
+}
