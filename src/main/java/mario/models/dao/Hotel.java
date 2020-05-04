afe1853dbf464e6ddde96b99c01b194e00a5c64a
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-05-05 00:34:12 +0200

    7ChangeServiceHotel

--- src/main/java/mario/models/dao/Hotel.java
+++ src/main/java/mario/models/dao/Hotel.java
@@ -1,13 +1,17 @@
-package mario.models;
+package mario.models.dao;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.GenericGenerators;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -21,6 +25,12 @@ public class Hotel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2",strategy="uuid2")
+    @Column(length = 36)
+    private String partnerCode;
 
     @NotNull
     private String title;
@@ -41,4 +51,10 @@ public class Hotel {
             inverseJoinColumns = @JoinColumn(name = "room_id"))
     private List<Room> rooms;
 
+
+    @CreationTimestamp
+    private LocalDateTime localDateTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updateDataTime;
 }
