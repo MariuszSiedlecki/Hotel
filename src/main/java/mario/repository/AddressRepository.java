ed8316c332643ddf94ff9bbf320ff315609bf6d9
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-05-19 23:27:00 +0200

    Tests

--- src/main/java/mario/repository/AddressRepository.java
+++ src/main/java/mario/repository/AddressRepository.java
@@ -2,10 +2,7 @@ package mario.repository;
 
 import mario.models.dao.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
